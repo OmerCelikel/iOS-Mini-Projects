b0485411b8442d56916d09e8699dc489adf990a7
Ömer <omeroguzcelikel@gmail.com>
2022-09-07 10:41:13 +0300

    createdNewProject

---
+++ slideGalary/slideGalary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  slideGalary
+//
+//  Created by Ömer Oğuz Çelikel on 7.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
