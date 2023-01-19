a6a6d48792d68bbef6334b109f6efe38f5f436cf
Omer Oguz Celikel <omer.celikel@d-teknoloji.com.tr>
2023-01-19 07:57:27 +0300

    Created new project for Login Screen

---
+++ iOSDev2_Designing a Login Screen/iOS Dev 2: Designing a Login Screen/iOS Dev 2: Designing a Login Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Dev 2: Designing a Login Screen
+//
+//  Created by Ömer Oğuz Çelikel on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
