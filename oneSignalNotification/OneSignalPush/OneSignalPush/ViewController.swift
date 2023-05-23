bee2939073e417981913863ed694afa47cd3322c
Omer Oguz Celikel <44753206+OmerCelikel@users.noreply.github.com>
2023-05-23 16:14:40 +0300

    push notification via FireBase

---
+++ oneSignalNotification/OneSignalPush/OneSignalPush/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneSignalPush
+//
+//  Created by Ömer Oğuz Çelikel on 23.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
